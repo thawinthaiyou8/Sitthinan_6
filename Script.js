d1269b542e65145a3f6d414c29ad97026b6606c4
thawinthaiyou8 <fanthaiyou@gmail.com>
2026-06-12 10:20:57 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,9 @@
-// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Portfolio ของ SITTHINAN พร้อมแล้ว!');
+
+    // ดึงปีปัจจุบัน (ค.ศ.) มาแสดงใน Footer อัตโนมัติ
+    const yearSpan = document.getElementById('year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
 });
